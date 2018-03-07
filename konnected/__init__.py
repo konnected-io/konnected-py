9ff61fd51e0fce88d81cd477fb97bd122301cfa1
Nate Clark <nate@nateclark.com>
2018-03-07 13:31:55 -0500

    implements client functions to get device status, pin status, and actuate an actuator

--- konnected/__init__.py
+++ konnected/__init__.py
@@ -16,6 +16,39 @@ class Client(object):
     self.port = port
     self.base_url = 'http://' + host + ':' + port
 
+  def get_device(self, pin=None):
+    ''' Query the status of a specific pin (or all configured pins if pin is ommitted) '''
+    url = self.base_url + '/device'
+    r = requests.get(url, params = {'pin':pin}, timeout = 30)
+    return r.json()
+
+  def get_status(self):
+    ''' Query the device status. Returns JSON of the device internal state '''
+    url = self.base_url + '/status'
+    r = requests.get(url, timeout = 30)
+    return r.json()
+
+  def put_device(self, pin, state, momentary=None, times=None, pause=None):
+    ''' Actuate a device pin '''
+    url = self.base_url + '/device'
+
+    payload = {
+      "pin": pin,
+      "state": state
+    }
+
+    if momentary is not None:
+      payload["momentary"] = momentary
+
+    if times is not None:
+      payload["times"] = times
+
+    if pause is not None:
+      payload["pause"] = pause
+
+    r = requests.put(url, json=payload, timeout = 30)
+    return r.ok
+
   def put_settings(self, sensors, actuators, auth_token, endpoint):
     ''' Sync settings to the Konnected device '''
     url = self.base_url + '/settings'
@@ -28,5 +61,4 @@ class Client(object):
     }
 
     r = requests.put(url, json=payload, timeout = 30)
-
     return r.ok
