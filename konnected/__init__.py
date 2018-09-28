4e111381608fd4303df4f03798065abbe3235ab8
Nate Clark <nate@nateclark.com>
2018-09-28 16:07:42 -0400

    add basic exception handling; formatting

--- konnected/__init__.py
+++ konnected/__init__.py
@@ -1,7 +1,5 @@
-''' Python library for interacting with Konnected devices '''
-'''            learn more at konnected.io                 '''
-
-__author__ = 'Nate Clark, Konnected Inc <help@konnected.io>'
+# Python library for interacting with Konnected devices
+# learn more at konnected.io
 
 import requests
 import json
@@ -9,56 +7,76 @@ import os
 
 from requests.exceptions import RequestException;
 
+__author__ = 'Nate Clark, Konnected Inc <help@konnected.io>'
+
+
 class Client(object):
 
-  def __init__(self, host, port):
-    self.host = host
-    self.port = port
-    self.base_url = 'http://' + host + ':' + port
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.base_url = 'http://' + host + ':' + port
 
-  def get_device(self, pin=None):
-    ''' Query the status of a specific pin (or all configured pins if pin is ommitted) '''
-    url = self.base_url + '/device'
-    r = requests.get(url, params = {'pin':pin}, timeout = 30)
-    return r.json()
+    def get_device(self, pin=None):
+        """ Query the status of a specific pin (or all configured pins if pin is ommitted) """
+        url = self.base_url + '/device'
+        try:
+            r = requests.get(url, params={'pin': pin}, timeout=10)
+            return r.json()
+        except RequestException as err:
+            raise Client.ClientError(err)
 
-  def get_status(self):
-    ''' Query the device status. Returns JSON of the device internal state '''
-    url = self.base_url + '/status'
-    r = requests.get(url, timeout = 30)
-    return r.json()
+    def get_status(self):
+        """ Query the device status. Returns JSON of the device internal state """
+        url = self.base_url + '/status'
+        try:
+            r = requests.get(url, timeout=10)
+            return r.json()
+        except RequestException as err:
+            raise Client.ClientError(err)
 
-  def put_device(self, pin, state, momentary=None, times=None, pause=None):
-    ''' Actuate a device pin '''
-    url = self.base_url + '/device'
+    def put_device(self, pin, state, momentary=None, times=None, pause=None):
+        """ Actuate a device pin """
+        url = self.base_url + '/device'
 
-    payload = {
-      "pin": pin,
-      "state": state
-    }
+        payload = {
+            "pin": pin,
+            "state": state
+        }
 
-    if momentary is not None:
-      payload["momentary"] = momentary
+        if momentary is not None:
+            payload["momentary"] = momentary
 
-    if times is not None:
-      payload["times"] = times
+        if times is not None:
+            payload["times"] = times
 
-    if pause is not None:
-      payload["pause"] = pause
+        if pause is not None:
+            payload["pause"] = pause
 
-    r = requests.put(url, json=payload, timeout = 30)
-    return r.json()
+        try:
+            r = requests.put(url, json=payload, timeout=10)
+            return r.json()
+        except RequestException as err:
+            raise Client.ClientError(err)
 
-  def put_settings(self, sensors, actuators, auth_token, endpoint):
-    ''' Sync settings to the Konnected device '''
-    url = self.base_url + '/settings'
+    def put_settings(self, sensors, actuators, auth_token, endpoint):
+        """ Sync settings to the Konnected device """
+        url = self.base_url + '/settings'
 
-    payload = {
-      "sensors": sensors,
-      "actuators": actuators,
-      "token": auth_token,
-      "apiUrl": endpoint
-    }
+        payload = {
+            "sensors": sensors,
+            "actuators": actuators,
+            "token": auth_token,
+            "apiUrl": endpoint
+        }
 
-    r = requests.put(url, json=payload, timeout = 30)
-    return r.ok
+        try:
+            r = requests.put(url, json=payload, timeout=30)
+            return r.ok
+        except RequestException as err:
+            raise Client.ClientError(err)
+
+    class ClientError(Exception):
+        """Generic Error."""
+        pass
+
