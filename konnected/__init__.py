2d70a7a9fa049b1f9ecc4120d0e2a3c51851b095
Kit Klein <kit@Kits-MBP.localdomain>
2019-12-29 11:27:08 -0500

    convert to async

--- konnected/__init__.py
+++ konnected/__init__.py
@@ -1,41 +1,39 @@
 # Python library for interacting with Konnected devices
 # learn more at konnected.io
-
-import requests
+import asyncio
+import aiohttp
 import json
 import os
 
-from requests.exceptions import RequestException;
-
 __author__ = 'Nate Clark, Konnected Inc <help@konnected.io>'
-
 
 class Client(object):
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, websession):
         self.host = host
         self.port = port
+        self.websession = websession
         self.base_url = 'http://' + host + ':' + port
 
-    def get_device(self, pin=None):
+    async def get_device(self, pin=None):
         """ Query the status of a specific pin (or all configured pins if pin is ommitted) """
         url = self.base_url + '/device'
         try:
-            r = requests.get(url, params={'pin': pin}, timeout=10)
-            return r.json()
-        except RequestException as err:
+            async with self.websession.get(url, params={'pin': pin}, timeout=10) as resp:
+                return await resp.json()
+        except aiohttp.ClientError as err:
             raise Client.ClientError(err)
 
-    def get_status(self):
+    async def get_status(self):
         """ Query the device status. Returns JSON of the device internal state """
         url = self.base_url + '/status'
         try:
-            r = requests.get(url, timeout=10)
-            return r.json()
-        except RequestException as err:
+            async with self.websession.get(url, timeout=10) as resp:
+                return await resp.json()
+        except aiohttp.ClientError as err:
             raise Client.ClientError(err)
 
-    def put_device(self, pin, state, momentary=None, times=None, pause=None):
+    async def put_device(self, pin, state, momentary=None, times=None, pause=None):
         """ Actuate a device pin """
         url = self.base_url + '/device'
 
@@ -54,12 +52,12 @@ class Client(object):
             payload["pause"] = pause
 
         try:
-            r = requests.put(url, json=payload, timeout=10)
-            return r.json()
-        except RequestException as err:
+            async with self.websession.put(url, json=payload, timeout=10) as resp:
+                return await resp.json()
+        except aiohttp.ClientError as err:
             raise Client.ClientError(err)
 
-    def put_settings(self, sensors=[], actuators=[], auth_token=None,
+    async def put_settings(self, sensors=[], actuators=[], auth_token=None,
                      endpoint=None, blink=None, discovery=None,
                      dht_sensors=[], ds18b20_sensors=[]):
         """ Sync settings to the Konnected device """
@@ -81,9 +79,9 @@ class Client(object):
             payload['discovery'] = discovery
 
         try:
-            r = requests.put(url, json=payload, timeout=10)
-            return r.ok
-        except RequestException as err:
+            async with self.websession.put(url, json=payload, timeout=10) as resp:
+                return await resp.json()
+        except aiohttp.ClientError as err:
             raise Client.ClientError(err)
 
     class ClientError(Exception):
