d810ad32004d9b5f2b005dea0e013730a54660b0
Kit Klein <kit@Kits-MBP.localdomain>
2020-01-10 13:20:50 -0500

    fix issues causing errors in pypi upload

--- setup.py
+++ setup.py
@@ -1,59 +1,31 @@
 #!/usr/bin/env python
 
-import os
-import sys
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-license = """
-MIT License
-Copyright (c) 2018 Konnected Inc
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
-long_description = """
-View details and usage examples in the `README.md <https://github.com/konnected-io/konnected-py> on GitHub`_.
-For device support see `help.konnected.io <https://help.konnected.io>`_.
-"""
+from setuptools import setup
 
 setup(
     name='konnected',
     version='1.0.0',
     packages=['konnected'],
     url='https://github.com/konnected-io/konnected-py',
-    license=license,
     description='A async Python library for interacting with Konnected home automation controllers (see https://konnected.io)',
-    long_description=long_description,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Nate Clark, Konnected Inc',
     author_email='help@konnected.io',
     install_requires=['aiohttp>=3.6.1'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License'
+    ],
     project_urls={
       'Homepage': 'https://github.com/konnected-io/konnected-py',
       'Website & Online Store': 'https://konnected.io',
-      'Help, Support and Community Forums': 'https://help.konnected.io',
-      'Facebook': 'https://facebook.com/konnected.io/',
+      'Support and Community Forums': 'https://help.konnected.io',
+      'Facebook': 'https://facebook.com/konnected.io',
       'YouTube Channel': 'https://youtube.com/c/Konnected'
     }
 )
